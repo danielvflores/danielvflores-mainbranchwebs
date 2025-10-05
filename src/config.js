9059c33ec6cc3bc21cd2bc8fc658e64c43dab6b8
Daniel Flores <datrixxacc@gmail.com>
2025-10-05 01:14:35 -0300

    wip: save local changes before merge with remote

---
+++ src/config.js
@@ -0,0 +1,34 @@
+export const SITE = {
+  title: 'Main Branch Webs - Daniel Flores Viera',
+  description: 'Desarrollador Backend — All info about Daniel Flores Viera',
+  keywords: 'Node.js, TypeScript, Java, Python',
+  author: 'Daniel Flores',
+  url: 'https://danielvflores.xyz',
+  lang: 'es',
+  locale: 'es_ES',
+  mainSocialName: '@danielvflores',
+  image: '/images/profile.jpg',
+}
+
+export const SOCIAL_LINKS = {
+    github : {
+      name: 'Github',
+      url: 'https://github.com/danielvflores',
+      icon: '/icons/github.svg',
+    },
+    linkedin : {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/danielvflores/',
+      icon: '/icons/linkedin.svg',
+    },
+    discord : {
+      name: 'Discord',
+      url: 'https://discord.com/users/835022014795874324',
+      icon: '/icons/discord.svg',
+    },
+    portfolio : {
+      name: 'Portfolio',
+      url: 'https://portfolio.danielvflores.xyz',
+      icon: '/icons/portfolio.svg',
+    }
+}
